5082bd9d927378cae0d532bc524e25690354ad14
LiangGamg <liang_game@sina.com>
2017-10-29 10:34:15 +0800

    王文亮 》》》 10.29

---
+++ src/pages/xiangq/xiangq.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { XiangqPage } from './xiangq';
+
+@NgModule({
+  declarations: [
+    XiangqPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(XiangqPage),
+  ],
+})
+export class XiangqPageModule {}
